5b526f1cce5a264e21dca0934febc7174afc68e1
Horse-Swimmer098 <66924507+Horse-Swimmer098@users.noreply.github.com>
2020-06-18 14:37:48 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 
-// This finction prints out your name and favorite color. 😍
+// This function prints out your name and favorite color. 😍
 function nameAndColor(name:string, color:string){
     console.log("Hello "+ name + ". Your favorite color is " + color)
 } 
@@ -7,9 +7,52 @@ function nameAndColor(name:string, color:string){
 nameAndColor("Kaathya", "Purple")
 
 
-// This finction prints out your name and favorite number.😍
+// This function prints out your name and favorite number. 😍
 function nameAndNumber(name:string, favNum:number){
     console.log("Hello " + name + ". Your favorite number is " + favNum)
 }
 
-nameAndNumber("Kaathya", 3)
+nameAndNumber ("Kaathya", 3)
+
+//This function prints out the grade and name. 😍
+
+function nameAndGrade(name:string, grade: number){
+    console.log("Kaathya , you are in " + grade)
+}
+nameAndGrade( "Kaathya", 6 )
+
+//This function prints out the name and school. 😍
+function nameAndSchool(name:string, school: string){
+    console.log("Hello " + name + " went to " + school )
+}
+nameAndSchool("Kaathya" , "Stevens Creek")
+
+//This function prints out the grade, name and school. 😍
+
+function nameGradeAndSchool (name:string, grade:number, school:string){
+    console.log("Hello " + name + ". You went to " + school + " and you were in " + grade )
+}
+nameGradeAndSchool("Kaathya" , 5 , "Stevens Creek")
+
+//This function multiplies a number by two. 😍
+function multiplyByTwo(startingNum:number){
+    let result = startingNum * 2
+    console.log(result)
+}
+
+multiplyByTwo(10000999)
+
+//This function multiplies a number by 12. 😍
+function multiplyByTwelve(startingNum:number){
+    let result = startingNum * 12
+    console.log(result)
+}
+multiplyByTwelve(4)
+
+//This function divides a number by two
+
+function divideByTwo(startingNum:number){
+    let result = startingNum / 2 
+    console.log(result)
+}
+divideByTwo(4)
