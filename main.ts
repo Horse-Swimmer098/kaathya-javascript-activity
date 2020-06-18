80ef18e0fd4109421d52339ea53e2a779928b9c2
Horse-Swimmer098 <66924507+Horse-Swimmer098@users.noreply.github.com>
2020-06-18 13:27:43 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+
+// This finction prints out your name and favorite color. 😍
+function nameAndColor(name:string, color:string){
+    console.log("Hello "+ name + ". Your favorite color is " + color)
+} 
+
+nameAndColor("Kaathya", "Purple")
+
+
+// This finction prints out your name and favorite number.😍
+function nameAndNumber(name:string, favNum:number){
+    console.log("Hello " + name + ". Your favorite number is " + favNum)
+}
+
+nameAndNumber("Kaathya", 3)
